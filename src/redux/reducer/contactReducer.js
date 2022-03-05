0d0ca5f02e23db417cc48ccb6aad9bfff980e97c
UdayShankhar <94035227+UdayShankhar@users.noreply.github.com>
2022-03-05 21:09:14 +0530

    Update contactReducer.js

--- src/redux/reducer/contactReducer.js
+++ src/redux/reducer/contactReducer.js
@@ -1,6 +1,6 @@
 const initialState = [
-    { id: 0, name: "Raman Sharma", email: "email@email.com", phone: 1234567890 },
-    { id: 1, name: "Test Name", email: "test@test.com", phone: 4567891230 },
+    { id: 0, name: "Test1", email: "email@email.com", phone: 1234567890 },
+    { id: 1, name: "Test2", email: "test@test.com", phone: 4567891230 },
 ];
 
  const contactReducer = (state = initialState, action) => {
